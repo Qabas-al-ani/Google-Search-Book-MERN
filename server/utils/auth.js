77e7fd9621fc4857646efe6af74b6ebc37406214
Qabas al ani <doctorqa@yahoo.com>
2021-10-23 18:48:22 -0400

    deletd res and next and added res.body.token to use ot later in the code

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,9 +6,9 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({req}) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token =req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -29,7 +29,8 @@ module.exports = {
     }
 
     // send to next endpoint
-    next();
+    // next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
