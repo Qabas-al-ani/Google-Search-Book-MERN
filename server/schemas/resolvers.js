4317dd755d20052d5dede47da394d440949ea814
Qabas al ani <doctorqa@yahoo.com>
2021-10-23 19:11:16 -0400

    created resolvers file that contain the Authentication error from apollo servers to show the error message set the queries for me and data errors checking if user exists with email and credentials and if not throw an error and same to add user and token and also to save book and remover book and then exported reslovers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,69 @@
+// importing Authentication error from apollo servers to show the error message
+const { AuthenticationError } = require("apollo-server-errors");
+// importing User from the modules
+const { User } = require("../models");
+// importing signToken from utils
+const { signToken } = require("../utils/auth");
+
+// set the queries for me and data errors
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
+      }
+      throw new AuthenticationError("Not logged in");
+    },
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      // checking if user exists with email and credentials
+      if (!user) {
+        //   if not throw an error
+        throw new AuthenticationError("Invalid credentials");
+      }
+
+      const correctPassword = await user.isCorrectPassword(password);
+      if (!correctPassword) {
+        throw new AuthenticationError("Invalid credentials");
+      }
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { input }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: input } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+};
+
+module.exports = resolvers;
