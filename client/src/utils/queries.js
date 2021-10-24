bfdba5f4a33b6426a28de8a0cc1391d3e7f08a5e
Qabas al ani <doctorqa@yahoo.com>
2021-10-23 21:42:35 -0400

    added the apollo provider and the apollo client from apollos

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+// import the graphql 
+import gql from "graphql-tag";
+
+// export GET_ME that contain me info dependencies
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        title
+        bookId
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+`;
