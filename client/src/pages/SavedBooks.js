1bab7b12ab445547b2663b750e4921b292ad86f0
Qabas al ani <doctorqa@yahoo.com>
2021-10-23 20:20:32 -0400

    refacterd the imports since we not gonna use use state and use effect any more

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,9 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import React from "react";
+import {
+  Jumbotron,
+  Container,
+  CardColumns,
+  Card,
+  Button,
+} from "react-bootstrap";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 
-import { getMe, deleteBook } from '../utils/API';
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
+import Auth from "../utils/auth";
+import { removeBookId, saveBookIds } from "../utils/localStorage";
+import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
