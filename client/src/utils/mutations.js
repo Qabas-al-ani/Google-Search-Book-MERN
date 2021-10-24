dc9227481bf1de3c3bd806a375638b250023ef1f
Qabas al ani <doctorqa@yahoo.com>
2021-10-23 21:26:52 -0400

    exprted the add user and the user login

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,28 @@
+// imported the gql for the mutation
 import gql from "graphql-tag";
+
+// export the login user
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+// export the add user
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+      token
+      user {
+        username
+        _id
+        email
+      }
+    }
+  }
+`;
